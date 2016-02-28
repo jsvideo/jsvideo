27e758eff8b988a09eb545ae77983f09d45393cf
Evgeny Poyarkov <eug.poyarkov@gmail.com>
2016-02-28 12:31:00 +0300

    Initial commit

---
+++ src/views/HomeView/HomeView.js
@@ -0,0 +1,14 @@
+/* @flow */
+import React from 'react'
+
+export class HomeView extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>Jsvideo</h1>
+      </div>
+    )
+  }
+}
+
+export default HomeView
